351f49c10a4bc4fc3c3ade11d41d5da1178668aa
DESKTOP-N62HNGM\Jose <jose.arranz.becerril.jab@gmail.com>
2020-01-29 13:09:55 +0100

    External url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   enviroment: "Produccion",
-  serviceUrl: "http://springcloud.jarranzz.svc",
+  serviceUrl: "http://springcloud-jarranzz.172.18.71.124.nip.io/",
   servicePath:"/api/hello",
-  serviceKafkaUrl :"http://springcloudkafka.jarranzz.svc",
+  serviceKafkaUrl :"http://angularcloudsample-jarranzz.172.18.71.124.nip.io/",
   servicekafkaPath:"/api/hello",
   servicekafkaRecivingPath:"/api/hellorecive"
 };
